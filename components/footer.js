03c4af36c1540435b7d57e7d5d6ec43cc0b01175
ohzw <52651644+ohzw@users.noreply.github.com>
2019-11-21 17:04:10 +0900

    Footer SNSリンク

--- components/footer.js
+++ components/footer.js
@@ -7,7 +7,13 @@ export default class extends React.Component {
       <div>
 
         <div className={scss.footer}>
-          <div className={scss.contact}><p>Contact</p></div>
+          <div className={scss.sns}>
+            <ul>
+              <li>Email</li>
+              <li>Twitter</li>
+              <li>Google</li>
+            </ul>
+          </div>
           <div className={scss.logo}><img src={require("../images/logo.png")}/></div>
           <div className={scss.copyright}><p>COPYRIGHT 2019</p></div>
         </div>
