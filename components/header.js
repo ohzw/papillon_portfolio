e7c9ba6ef6d3dce8b43329da5aeb68039ccdfffb
7esto <52750462+7esto@users.noreply.github.com>
2019-12-13 23:28:29 +0900

    est

--- components/header.js
+++ components/header.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import scss from "../components/header.scss"
+import Head from 'next/head'
 
 export default class extends React.Component {
   render() {
     return(
       <div>
+        <head>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.2/animate.min.css"></link>
+
+        </head>
         <div className={scss.header_title}>
-          <h1>PAPILLON</h1>
+          <h1 class="animated fadeInDown">PAPILLON</h1>
           <p>個人が輝ける未来への挑戦</p>
         </div>
       </div>
--- components/page_top.js
+++ components/page_top.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import scss from "../components/page_top.scss"
+import Head from 'next/head'
 
 export default class extends React.Component {
   render() {
     return(
       <div>
+        <Head>
+         <script type="text/JavaScript" src="page_anime.js"></script>
+        </Head>
+       
         <div className={scss.page_top}>
-          <a href="#">PAGE TOP ▲</a>
+          <a href="#" id="return-top">PAGE TOP ▲</a>
         </div>
       </div>
     )
