c39539d215d9659171d3b9d0ee1baa7a15fbd33c
ohzw <52651644+ohzw@users.noreply.github.com>
2019-12-29 20:24:33 +0900

    header menu animation

--- static/script.js
+++ static/script.js
@@ -1,26 +1,25 @@
 function topButton(elmId, duration) {
 
-  //トップに戻るボタンの要素の取得
-  var topButton = document.getElementById(elmId);
+//トップに戻るボタンの要素の取得
+var topButton = document.getElementById(elmId);
 
-  topButton.addEventListener("click", function(e){
+topButton.addEventListener("click", function(e){
 
-      //デフォルトの動作の制御
-      e.preventDefault();
+    //デフォルトの動作の制御
+    e.preventDefault();
 
-      var begin = new Date() - 0;
-      var yOffset = window.pageYOffset;
-      var timer= setInterval(function() {
-          var current = new Date() - begin;
-          if (current > duration) {
-              clearInterval(timer);
-              current = duration;
-          }
-
-          //スクロール位置を単位時間で変更する
-          window.scrollTo(0, yOffset * (1 - current / duration)) 
-      }, 10);
-  }, false)
+    var begin = new Date() - 0;
+    var yOffset = window.pageYOffset;
+    var timer= setInterval(function() {
+        var current = new Date() - begin;
+        if (current > duration) {
+            clearInterval(timer);
+            current = duration;
+        }
+        //スクロール位置を単位時間で変更する
+        window.scrollTo(0, yOffset * (1 - current / duration)) 
+    }, 10);
+}, false)
 
 }
 
